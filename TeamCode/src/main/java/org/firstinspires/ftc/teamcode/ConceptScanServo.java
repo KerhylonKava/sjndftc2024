699182a1403c58806e48c0eef073ca7c14ec1cb8
RoboticsTeamContributeridrk <fmartin27@sjnd.org>
2024-12-03 17:04:11 -0800

    Added servo code - Felix - code of "main nov 2024"

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConceptScanServo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConceptScanServo.java
@@ -27,89 +27,91 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package org.firstinspires.ftc.robotcontroller.external.samples;
+package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
 
-/*
- * This OpMode scans a single servo back and forward until Stop is pressed.
- * The code is structured as a LinearOpMode
- * INCREMENT sets how much to increase/decrease the servo position each cycle
- * CYCLE_MS sets the update period.
- *
- * This code assumes a Servo configured with the name "left_hand" as is found on a Robot.
- *
- * NOTE: When any servo position is set, ALL attached servos are activated, so ensure that any other
- * connected servos are able to move freely before running this test.
- *
- * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
- */
-@TeleOp(name = "Concept: Scan Servo", group = "Concept")
-//@Disabled
-public class ConceptScanServo extends LinearOpMode {
+@TeleOp(name = "mainnov2024 (Blocks to Java)")
+public class mainnov2024 extends LinearOpMode {
 
-    static final double INCREMENT   = 0.01;     // amount to slew servo each CYCLE_MS cycle
-    static final int    CYCLE_MS    =   50;     // period of each cycle
-    static final double MAX_POS     =  1.0;     // Maximum rotational position
-    static final double MIN_POS     =  0.0;     // Minimum rotational position
+  private Servo left_hand;
 
-    // Define class members
-    Servo   servo;
-    double  position = (MAX_POS - MIN_POS) / 2; // Start at halfway position
-    boolean rampUp = true;
+  double gripper_lateral_pos;
 
+  /**
+   * This sample contains the bare minimum Blocks for any regular OpMode. The 3 blue
+   * Comment Blocks show where to place Initialization code (runs once, after touching the
+   * DS INIT button, and before touching the DS Start arrow), Run code (runs once, after
+   * touching Start), and Loop code (runs repeatedly while the OpMode is active, namely not
+   * Stopped).
+   */
+  @Override
+  public void runOpMode() {
+    left_hand = hardwareMap.get(Servo.class, "left_hand");
 
-    @Override
-    public void runOpMode() {
-
-        // Connect to servo (Assume Robot Left Hand)
-        // Change the text in quotes to match any servo name on your robot.
-        servo = hardwareMap.get(Servo.class, "left_hand");
-
-        // Wait for the start button
-        telemetry.addData(">", "Press Start to scan Servo." );
+    // Made by Felix, ideas from whoever made the old main
+    // Make sure to press START + A when the gamepad is connected to set it to gamepad1
+    waitForStart();
+    if (opModeIsActive()) {
+      gripper_lateral_pos = 1;
+      while (opModeIsActive()) {
         telemetry.update();
-        waitForStart();
-
-
-        // Scan servo till stop pressed.
-        while(opModeIsActive()){
-
-            // slew the servo, according to the rampUp (direction) variable.
-            if (rampUp) {
-                // Keep stepping up until we hit the max value.
-                position += INCREMENT ;
-                if (position >= MAX_POS ) {
-                    position = MAX_POS;
-                    rampUp = !rampUp;   // Switch ramp direction
-                }
-            }
-            else {
-                // Keep stepping down until we hit the min value.
-                position -= INCREMENT ;
-                if (position <= MIN_POS ) {
-                    position = MIN_POS;
-                    rampUp = !rampUp;  // Switch ramp direction
-                }
-            }
-
-            // Display the current value
-            telemetry.addData("Servo Position", "%5.2f", position);
-            telemetry.addData(">", "Press Stop to end test." );
-            telemetry.update();
-
-            // Set the servo to the new position and pause;
-            servo.setPosition(position);
-            sleep(CYCLE_MS);
-            idle();
-        }
-
-        // Signal done;
-        telemetry.addData(">", "Done");
-        telemetry.update();
+        left_hand.scaleRange(0.4, 1);
+        move_gripper_laterally();
+        open_gripper();
+        close_gripper();
+      }
     }
+  }
+
+  /**
+   * changes the gripper to close or open
+   */
+  private void move_gripper_laterally() {
+    while (gamepad1.a && gripper_lateral_pos > 0.4) {
+      gripper_lateral_pos += -0.0037;
+      left_hand.setPosition(gripper_lateral_pos);
+    }
+    while (gamepad1.b && gripper_lateral_pos < 1) {
+      gripper_lateral_pos += 0.0037;
+      left_hand.setPosition(gripper_lateral_pos);
+    }
+  }
+
+  /**
+   * opens gripper
+   */
+  private void open_gripper() {
+    if (gamepad1.left_bumper) {
+      gripper_lateral_pos = 0.4;
+      left_hand.setPosition(gripper_lateral_pos);
+    }
+  }
+
+  /**
+   * closes gripper
+   */
+  private void close_gripper() {
+    if (gamepad1.right_bumper) {
+      gripper_lateral_pos = 1;
+      left_hand.setPosition(gripper_lateral_pos);
+    }
+  }
+
+  /**
+   * does what the name tells you
+   */
+  private void slowly_close_then_stop_gripper() {
+    if (gamepad1.dpad_right) {
+      while (!!gamepad1.dpad_right) {
+      }
+      while (!(gamepad1.dpad_right || gripper_lateral_pos == 1 || !opModeIsActive())) {
+        gripper_lateral_pos += 0.0013;
+        left_hand.setPosition(gripper_lateral_pos);
+      }
+      gripper_lateral_pos = 1;
+    }
+  }
 }
